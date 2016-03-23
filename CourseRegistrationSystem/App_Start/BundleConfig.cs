9bcb6002b7cbb92ff49b9449fb856e8475ebafcc
intelbrainee <kennedy20000@yahoo.com>
2016-03-23 08:33:01 +0100

    Initial Commit

---
+++ CourseRegistrationSystem/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+
+namespace CourseRegistrationSystem
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+        }
+    }
+}
---
+++ CourseRegistrationSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CourseRegistrationSystem.MvcApplication" Language="C#" %>
