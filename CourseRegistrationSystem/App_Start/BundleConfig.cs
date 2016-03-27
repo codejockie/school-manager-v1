9a78f9066cecdefd796d5ab50e5bf2949cf8e465
intelbrainee <kennedy20000@yahoo.com>
2016-03-27 18:25:37 +0100

    Imported styles and scripts (jQuery and bootstrap)

--- CourseRegistrationSystem/App_Start/BundleConfig.cs
+++ CourseRegistrationSystem/App_Start/BundleConfig.cs
@@ -11,7 +11,25 @@ namespace CourseRegistrationSystem
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
 
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-2.2.1.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive")
+                .Include("~/scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-2.2.1.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive")
+                .Include("~/scripts/bootstrap.js"));
         }
     }
 }
